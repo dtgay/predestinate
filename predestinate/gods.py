5e8b47d015ba532da3d432242cf45b9827d0b3b7
David Gay <oddshocks@riseup.net>
2013-04-09 19:01:14 -0400

    who is to say that there can be only one of each god?

--- predestinate/gods.py
+++ predestinate/gods.py
@@ -4,7 +4,6 @@ import subprocess # to run xautomation
 class MouseGod(object):
     """Can predestine the actions of the mouse."""
 
-    @classmethod
     def move_mouse_abs(cls, x, y):
         """Move the mouse pointer absolutely
         x: change in x
@@ -12,7 +11,6 @@ class MouseGod(object):
         xte_command = "mousemove {} {}".format(x, y)
         subprocess.call(["xte", xte_command])
 
-    @classmethod
     def move_mouse_rel(cls, x, y):
         """Move the mouse pointer relatively
         x: change in x
